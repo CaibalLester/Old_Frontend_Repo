53dd417b5a6f91c41f4fa521f6ae5ed82a541116
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2023-11-17 22:22:07 +0800

    reg validation

--- src/views/LogIn/Register.vue
+++ src/views/LogIn/Register.vue
@@ -1,14 +1,14 @@
 <template>
     <section id="loginhero" class="loginhero d-flex align-items-center">
 
-        
-<div class="card">
-  <div class="mycontainer">
 
-    
-            <div class="row justify-content-center">
-                <div class="col-md-12 col-lg-10">
-              
+        <div class="card">
+            <div class="mycontainer">
+
+
+                <div class="row justify-content-center">
+                    <div class="col-md-12 col-lg-10">
+
                         <div class="img" :style="imgStyle"></div>
                         <div class="login-wrap p-4 p-md-5">
                             <div class="d-flex">
@@ -29,25 +29,36 @@
                                 </div>
                                 <div class="form-group mb-3">
                                     <label class="label" for="password">Password</label>
-                                    <input type="password" class="form-control" placeholder="Password" v-model="password" required>
+                                    <input type="password" class="form-control" placeholder="Password" v-model="password"
+                                        required>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label class="label" for="confirm">Confirm Password</label>
-                                    <input type="password" class="form-control" placeholder="Confirm Password" v-model="confirm" required>
+                                    <input type="password" class="form-control" placeholder="Confirm Password"
+                                        v-model="confirm" required>
+                                    <div v-if="passwordMismatch" class="text-danger mt-2">
+                                        Passwords do not match.
+                                    </div>
+                                    <div v-if="password.length > 0 && password.length < 6" class="text-danger mt-2">
+                                        Password must be at least 6 characters long.
+                                    </div>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label class="label" for="role">Role</label>
                                     <select class="form-select" v-model="role" required>
-                                        <option selected value="">Applicant</option>
+                                        <option value="">Select Role</option>
+                                        <option selected value="applicant">Applicant</option>
                                         <option value="admin">Admin</option>
                                         <option value="agent">Agent</option>
                                     </select>
                                 </div>
 
+
                                 <div class="form-group">
-                                    <button type="submit" class="form-control btn btn-dark rounded submit px-3" href="">Sign Up</button>
+                                    <button type="submit" class="form-control btn btn-dark rounded submit px-3" href="">Sign
+                                        Up</button>
                                 </div>
-                               
+
                                 <div class="form-group d-md-flex">
                                     <div class="w-50 text-left">
                                         <label class="checkbox-wrap checkbox-dark mb-0">Remember Me
@@ -57,13 +68,13 @@
                                     </div>
                                 </div>
                             </form>
-                          
+
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 </template>
 
 <script>
@@ -82,20 +93,42 @@ export default {
             password: "",
             confirm: "",
             role: "",
-            rememberMe: false
+            rememberMe: false,
+            passwordMismatch: false,
         };
     },
     methods: {
         async save() {
+            // Check if passwords match
+            if (this.password !== this.confirm) {
+                this.passwordMismatch = true;
+                return; // Do not proceed with the save method if passwords don't match
+            } else {
+                // Reset the passwordMismatch flag if passwords match
+                this.passwordMismatch = false;
+            }
+
+            // Check if password meets the minimum length requirement
+            if (this.password.length < 6) {
+                // Display an error message or take appropriate action
+                console.log("Password must be at least 6 characters long");
+                return;
+            }
+
             try {
                 const ins = await axios.post('authreg', {
                     email: this.email,
                     password: this.password,
                     role: this.role,
                 });
-                this.email =""
-                this.password ="";
-                this.confirm ="";
+
+                // Redirect to login page after successful registration
+                this.$router.push('/login');
+
+                // Clear form fields after successful registration
+                this.email = "";
+                this.password = "";
+                this.confirm = "";
             } catch (error) {
                 console.log(error);
             }
